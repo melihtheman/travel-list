47f1fb7f8acc7018d35077b147ddfe100bd5dfe2
Melih Furkan Aydan <B210109038@SUBU.EDU.TR>
2024-11-22 12:06:39 +0300

    https://github.com/melihtheman/react-travel-list.git

---
+++ src/components/App.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import Logo from "../components/Logo";
+import Form from "../components/Form";
+import PackingList from "../components/PackingList";
+import Stats from "../components/Stats";
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
+export default function App() {
+  const [items, setItems] = useState(initialItems);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
+  function handleClearList() {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete all items?"
+    );
+    if (confirmed) setItems([]);
+  }
+  return (
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItems={handleToggleItem}
+        onDeleteItems={handleClearList}
+      />
+      <Stats items={items} />
+    </div>
+  );
+}
